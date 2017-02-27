e208bdaa902f463a8da8653d14bbd46abaab8471
FengChueng <yinglongChueng@gmail.com>
2017-02-27 22:39:24 +0800

    init

---
+++ app/src/main/java/com/zyl/hospital/registration/base/BaseFragment.java
@@ -0,0 +1,9 @@
+package com.zyl.hospital.registration.base;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by zhangyinglong on 2017/2/21.
+ */
+public class BaseFragment extends Fragment {
+}
---
+++ app/src/main/java/com/zyl/hospital/registration/base/BaseView.java
@@ -0,0 +1,7 @@
+package com.zyl.hospital.registration.base;
+
+/**
+ * Created by zhangyinglong on 2017/2/21.
+ */
+public class BaseView {
+}
